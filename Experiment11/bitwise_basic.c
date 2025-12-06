12be481fe82ea663388943cceb851fa5c7db2ab7
aman-barnwal <lalionare@gmail.com>
2025-12-06 19:40:57 +0000

    Updated Lab Files

---
+++ Experiment11/bitwise_basic.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int a = 10, b = 6;
+
+    printf("a AND b = %d\n", a & b);
+    printf("a OR b  = %d\n", a | b);
+    printf("NOT a   = %d\n", ~a);
+
+    return 0;
+}
+
---
+++ Experiment11/shift.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int x = 8;
+
+    printf("Left Shift (8 << 2) = %d\n", x << 2);
+    printf("Right Shift (8 >> 2) = %d\n", x >> 2);
+
+    return 0;
+}
+
